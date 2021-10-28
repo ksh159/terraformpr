bae156850597867a774ab115286bf3fe73891673
ksh159 <xndb15915@gmail.com>
2021-10-28 16:21:48 +0900

    step7

---
+++ 07_nga.tf
@@ -0,0 +1,24 @@
+resource "aws_eip" "lb_ip_a" {
+  vpc = true
+}
+
+resource "aws_eip" "lb_ip_c" {
+#   instance = aws_instance.web.id
+  vpc = true
+}
+
+resource "aws_nat_gateway" "ksh_nga_a" {
+  allocation_id = aws_eip.lb_ip_a.id
+  subnet_id = aws_subnet.ksh-pria.id
+  tags = {
+    Name = "ksh-nga-a"
+  }
+}
+
+resource "aws_nat_gateway" "ksh_nga_c" {
+  allocation_id = aws_eip.lb_ip_c.id
+  subnet_id = aws_subnet.ksh-pric.id
+  tags = {
+    Name = "ksh-nga-c"
+  }
+}
