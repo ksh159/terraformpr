bd525d0d6114e867cd563c71d1a487e001583f7e
ksh159 <xndb15915@gmail.com>
2021-10-28 16:30:42 +0900

    step0306

--- 03_sub.tf
+++ 03_sub.tf
@@ -1,36 +1,43 @@
 # 가용영역 a의 Public Subnet
-resource "aws_subnet" "ksh_puba" {
-  vpc_id = aws_vpc.ksh_vpc.id
-  cidr_block = "10.0.0.0/24"
+resource "aws_subnet" "ksh-puba" {
+  vpc_id            = aws_vpc.ksh_vpc.id
+  cidr_block        = "10.0.0.0/24"
   availability_zone = "ap-northeast-2a"
+
   tags = {
-    "Name" = "ksh-puba"
+    Name = "ksh-puba"
   }
 }
 
-resource "aws_subnet" "ksh_pria" {
-  vpc_id = aws_vpc.ksh_vpc.id
-  cidr_block = "10.0.1.0/24"
+# 가용영역 a의 Private Subnet
+resource "aws_subnet" "ksh-pria" {
+  vpc_id            = aws_vpc.ksh_vpc.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-northeast-2a"
+
   tags = {
-    "Name" = "ksh-pria"
+    Name = "ksh-pria"
   }
 }
 
-resource "aws_subnet" "ksh_pubc" {
-  vpc_id = aws_vpc.ksh_vpc.id
-  cidr_block = "10.0.2.0/24"
+# 가용영역 c의 Public Subnet
+resource "aws_subnet" "ksh-pubc" {
+  vpc_id            = aws_vpc.ksh_vpc.id
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "ap-northeast-2c"
+
   tags = {
-    "Name" = "ksh-pubc"
+    Name = "ksh-pubc"
   }
 }
 
-resource "aws_subnet" "ksh_pric" {
-  vpc_id = aws_vpc.ksh_vpc.id
-  cidr_block = "10.0.3.0/24"
+# 가용영역 c의 Private Subnet
+resource "aws_subnet" "ksh-pric" {
+  vpc_id            = aws_vpc.ksh_vpc.id
+  cidr_block        = "10.0.3.0/24"
   availability_zone = "ap-northeast-2c"
+
   tags = {
-    "Name" = "ksh-pric"
+    Name = "ksh-pric"
   }
 }
--- 06_rtas.tf
+++ 06_rtas.tf
@@ -1,9 +1,9 @@
 resource "aws_route_table_association" "ksh_rtas_a" {
-  subnet_id = aws_subnet.ksh_puba.id
+  subnet_id = aws_subnet.ksh-puba.id
   route_table_id = aws_route_table.ksh_rt.id
 }
 
 resource "aws_route_table_association" "ksh_rtas_c" {
-  subnet_id = aws_subnet.ksh_pubc.id
+  subnet_id = aws_subnet.ksh-pubc.id
   route_table_id = aws_route_table.ksh_rt.id
 }
